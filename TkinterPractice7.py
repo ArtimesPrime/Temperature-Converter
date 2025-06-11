9bae1aca38a3bf59fcfeaec1e8d365eeb8962183
ArtimesPrime <eb20190405@gmail.com>
2025-06-11 13:38:32 +1200

    Add files via upload
    Menu works and you can navigate

--- TkinterPractice7.py
+++ TkinterPractice7.py
@@ -34,9 +34,41 @@ class Converter:
 
     def create_main_frame(self):
         frame = Frame(self.container)
+
+        self.Temperature_Converter = Label(frame, font = "Verdana 16 bold", text="Temperature converter")
+        self.Temperature_Converter.grid(row=0, columnspan=2)
+
+        self.Navigate1 = Button(frame, text ="To Centigrade", bg="yellow", font="Verdana 12 bold", command=lambda:self.show_frame("F_to_C"))
+        self.Navigate1.grid(row=1, column=0,padx=10,pady=10)
+    
+        self.Navigate2 = Button(frame, text ="To Fahrenheit", bg="Red", font="Verdana 12 bold", command=lambda:self.show_frame("C_to_F"))
+        self.Navigate2.grid(row=1, column=1,padx=10,pady=10)
+    
+        frame.grid(row=0)
+        return frame
+
+    def create_c_to_f(self):
+        frame = Frame(self.container)
+
+        self.title = Label(frame, font = "Verdana 16 bold", text="Farenheit Converter")
+        self.title.grid(row=0, columnspan=2)
+
+        self.entry = Entry(frame, justify=CENTER)
+        self.entry.grid(row=1, columnspan=2)
+
         
-        self.Temperature_Converter = Label(Frame, font = "Verdana 16 Bold", text="Temperature converter")
-        self.grid(row=0, columnspan=2)
+
+
+
+        frame.grid(row=0)
+        return frame
+    
+    def create_f_to_c(self):
+        pass
+    
+
+    def Centigrade_TO_Farenheiht(self):
+        self.farenheit = int(self.entry.get())
 
 if __name__ == "__main__":
     app = Converter()
